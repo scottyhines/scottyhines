fe5a8113d2a8e78088f25712afc577f738f51a04
scottyhines <60279014+scottyhines@users.noreply.github.com>
2024-06-18 08:04:32 -0700

    Add files via upload

--- tutorial - scottyhines.R
+++ tutorial - scottyhines.R
@@ -7,8 +7,13 @@ df <- scottyhines::create_fake_data(5000)
 df %>% glimpse()
 
 outcomes <- c("attitude_work_ethic", "attitude_adaptability", "attitude_creativity")
-vars_of_interest <- c("attitude_teamwork", "attitude_communication", "demo_job_level", "attitude_persistence")
+vars_of_interest <- c("attitude_teamwork", "attitude_communication", "attitude_persistence")
 
+# Tables
+scottyhines::build_means_table(df, group_var = "demo_performance", vars = outcomes)
+scottyhines::build_correlation_table(data = df, outcome_vars = outcomes, interest_vars = vars_of_interest)
+
+# analyses
 out_ttest <- scottyhines::run_t_test(df, outcome_vars = outcomes, group_var = "demo_attrition")
 out_anova <- scottyhines::run_anova_with_contrasts(df, outcome_vars = outcomes, control_vars = "attitude_leadership",  group_var = "demo_region_code")
 out_anova$anova_summary
@@ -19,5 +24,4 @@ scottyhines::run_hierarchical_regression(df, outcome_var = "demo_performance", c
 scottyhines::run_network_analysis(df, outcome_var = "demo_performance", control_vars = "attitude_leadership", interest_vars = c("network_degree", "network_betweenness", "network_closeness", "network_eigenvector"), group_var = "demo_job_level", num_permutations = 500)
 scottyhines::run_heartbeat(df, outcomes)
 scottyhines::run_causal_attitude_network(df, gamma = .5, threshold = F)
-scottyhines::build_means_table(df, group_var = "demo_performance", vars = outcomes)
-scottyhines::run_mixed_effects_model(data = df, outcome_vars = "attitude_work_ethic", control_vars = NULL, interest_vars = "network_betweenness", random_effects = "(1 | demo_job_level)", interaction_var = "demo_attrition", include_interaction = T)
+scottyhines::run_mixed_effects_model(df, outcome_vars = c("attitude_work_ethic", "attitude_adaptability"), control_vars = c("attitude_teamwork"), interest_vars = c("attitude_communication"), random_effects = "(1 | demo_job_level)", interaction_var = "demo_job_level", include_interaction = T)
